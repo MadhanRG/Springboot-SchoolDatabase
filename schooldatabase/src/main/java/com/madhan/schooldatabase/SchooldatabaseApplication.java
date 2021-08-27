404cd3d680b33f08190f0786b060401d3c875fe9
Madhan <madhankumar.rg@gmail.com>
2021-08-27 02:20:19 -0700

    Yes only in sout

--- schooldatabase/src/main/java/com/madhan/schooldatabase/SchooldatabaseApplication.java
+++ schooldatabase/src/main/java/com/madhan/schooldatabase/SchooldatabaseApplication.java
@@ -8,6 +8,8 @@ public class SchooldatabaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchooldatabaseApplication.class, args);
+
+		System.out.println("Its for git application bhul");
 	}
 
 }
